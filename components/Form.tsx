5c7891aedec3cc6861303dc2ca7e7c795e4391d6
Brian Yu <brian@Brians-MacBook-Air.local>
2024-04-06 09:28:57 -0500

    a

--- components/Form.tsx
+++ components/Form.tsx
@@ -11,7 +11,7 @@ const findRequestURL = (mail: string) => {
   const accountID = getNumbers[0];
   const formID = getNumbers[1];
 
-  const requestURL = `https://assets.mailerlite.com/jsonp/${accountID}/forms/${formID}/subscribe?fields[email]=${mail}`;
+  const requestURL = `https://docs.google.com/forms/d/e/1FAIpQLSfymV3qOO4JyskApDWxmUmaKOikGu22pigf7GqUKiG3BrfYZQ/viewform?usp=pp_url&entry.1352467152={formID}`;
 
   return requestURL;
 };
