6eeafd911fb832270d9c9ba7902df3782c7880a7
Brian Yu <brian@Brians-MacBook-Air.local>
2024-04-06 11:51:31 -0500

    a

--- components/Form.tsx
+++ components/Form.tsx
@@ -12,9 +12,9 @@ const findRequestURL = (mail: string) => {
   const formID = getNumbers[1];
 
   mail = JSON.stringify(mail)
-  mail = encodeURIComponent(mail)
+  //mail = encodeURIComponent(mail)
 
-  const requestURL = `https://corsproxy.io/?https://docs.google.com/forms/d/e/1FAIpQLSfymV3qOO4JyskApDWxmUmaKOikGu22pigf7GqUKiG3BrfYZQ/formResponse?&submit=Submit?usp=pp_url&entry.1352467152=${mail}`;
+  const requestURL = 'https://corsproxy.io/?' + encodeURIComponent('https://docs.google.com/forms/d/e/1FAIpQLSfymV3qOO4JyskApDWxmUmaKOikGu22pigf7GqUKiG3BrfYZQ/formResponse?&submit=Submit?usp=pp_url&entry.1352467152=' + mail);
   
   return requestURL;
 };
